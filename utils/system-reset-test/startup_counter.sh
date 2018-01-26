b7931d0ab242f7f8c69c88c6911eccd3468bc87b
Liao Jieliang <jack@tronlong.com>
2018-01-26 16:54:45 +0800

    Add utils/system-reset-test/startup_counter.sh

---
+++ utils/system-reset-test/startup_counter.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -e /home/root/startup_counter.txt ]
+then
+    COUNT0=`head -n 1 /home/root/startup_counter.txt`
+    COUNT=`expr ${COUNT0} + 1`
+    echo ${COUNT} > /home/root/startup_counter.txt
+    echo 'Number of start times' >> /home/root/startup_counter.txt
+    echo 'System has started count:'
+    echo ${COUNT}
+else
+    echo '1' > /home/root/startup_counter.txt
+    echo 'Number of start times' >> /home/root/startup_counter.txt
+    echo 'The first time to start up this system!'
+fi
