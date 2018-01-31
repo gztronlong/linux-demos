648bec174e5b06019eaced53bd81490eac6037c8
Liao Jieliang <jack@tronlong.com>
2018-01-31 17:34:11 +0800

    Update startup_counter.sh

--- utils/system-reset-test/startup_counter.sh
+++ utils/system-reset-test/startup_counter.sh
@@ -2,6 +2,7 @@
 
 if [ -e /home/root/startup_counter.txt ]
 then
+    date '+%F %T' >> /home/root/startup_time.txt
     COUNT0=`head -n 1 /home/root/startup_counter.txt`
     COUNT=`expr ${COUNT0} + 1`
     echo ${COUNT} > /home/root/startup_counter.txt
@@ -9,6 +10,7 @@ then
     echo 'System has started count:'
     echo ${COUNT}
 else
+    date '+%F %T' >> /home/root/startup_time.txt
     echo '1' > /home/root/startup_counter.txt
     echo 'Number of start times' >> /home/root/startup_counter.txt
     echo 'The first time to start up this system!'
